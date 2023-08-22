5f51903659943525fb65d4cca657c472551a2745
JeanPaulRF <64498696+JeanPaulRF@users.noreply.github.com>
2023-08-21 23:13:18 -0600

    login style

--- FrontEnd/consultec/src/views/LoginView.js
+++ FrontEnd/consultec/src/views/LoginView.js
@@ -1,22 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/LoginStyle.css';
 
 function LoginView({ username, password, onUsernameChange, onPasswordChange, onLogin }) {
     return (
-        <div>
-            <h2>Login</h2>
-            <input
-                type="text"
-                placeholder="Username"
-                value={username}
-                onChange={(e) => onUsernameChange(e.target.value)}
-            />
-            <input
-                type="password"
-                placeholder="Password"
-                value={password}
-                onChange={(e) => onPasswordChange(e.target.value)}
-            />
-            <button onClick={onLogin}>Login</button>
+        <div className="login-container">
+            <div className="login-form">
+                <h2>Login</h2>
+                <form>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={username}
+                        onChange={(e) => onUsernameChange(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={(e) => onPasswordChange(e.target.value)}
+                    />
+                    <button onClick={onLogin}>Login</button>
+                </form>
+            </div>
         </div>
     );
 }
