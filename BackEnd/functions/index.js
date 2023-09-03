558a244a0c9a34e01c603e49599fe39a5e029cff
Esteban <estebanguzra@gmail.com>
2023-09-02 23:04:16 -0600

    ADD users in functions and firestore

--- BackEnd/functions/index.js
+++ BackEnd/functions/index.js
@@ -6,14 +6,43 @@
  *
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
+const firebaseConfig  = require('./firebaseConfig');
+const { getFirestore, collection, addDoc, connectFirestoreEmulator } = require('firebase/firestore');
+const { initializeApp } = require('firebase/app');
 
 const {onRequest} = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const express = require('express');
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const app = express();
+const firebaseApp = initializeApp(firebaseConfig)
+const db = getFirestore(firebaseApp);
+// Utilizar el emulador de Firestore en localhost
+connectFirestoreEmulator(db, '127.0.0.1', 8080);
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+app.get('/:id', (req, res) => res.send(`Hola ${req.params.id}`));
+
+app.post('/users/', (req, res) => {
+  try{
+    const usersRef = collection(db, 'users');
+    const docData = {
+      name: req.body.name,
+      email: req.body.email,
+      career: req.body.career
+    };
+    addDoc(usersRef, docData)
+      .then((docRef) => {
+        res.send('Document written with ID: ' + docRef.id);
+      })
+      .catch((error) => {
+        res.send('Error adding document: ' + error.message);
+      })
+  } catch (error) {
+    res.send('Error with the data sent: ' + error);
+  } 
+}
+);
+
+
+// Expose Express API as a single Cloud Function:
+exports.api = onRequest(app);
