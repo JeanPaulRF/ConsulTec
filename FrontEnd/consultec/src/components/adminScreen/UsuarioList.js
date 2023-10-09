e5ac8c44a0b8a53c99928e08583ee2f22bc375c2
RonaldCF <ronaldcordero32@gmail.com>
2023-10-08 22:08:05 -0600

    correcciones usuarioslist

--- FrontEnd/consultec/src/components/adminScreen/UsuarioList.js
+++ FrontEnd/consultec/src/components/adminScreen/UsuarioList.js
@@ -1,24 +1,30 @@
 import React, { useState, useEffect } from 'react';
 import { app } from '../../firebaseConfig';
 import { getAuth, deleteUser } from 'firebase/auth';
-import { collection, query, getDocs, doc, deleteDoc} from "firebase/firestore";
+import { collection, query, getDocs, doc, deleteDoc } from "firebase/firestore";
 import { db } from '../../firebaseConfig';
 
-const UserList = ( {refreshList}) => {
+const UserList = ({ refreshList, refreshDynamicDisplayList }) => {
 
   const [users, setUsers] = useState([]);
+  const [showRow, setShowRow] = useState(true);
+  const [showButton, setShowButton] = useState(false);
+
+
 
   useEffect(() => {
     const fetchData = async () => {
-    let userlist = []  
+
       try {
         const q = query(collection(db, "users"));
 
         const querySnapshot = await getDocs(q);
+        let userlist = []
         querySnapshot.forEach((doc) => {
-          userlist.push( {id: doc.id, ...doc.data()})
+          userlist.push({ id: doc.id, ...doc.data() })
         });
         setUsers(userlist);
+
       }
       catch (error) {
         console.log(error)
@@ -46,36 +52,37 @@ const UserList = ( {refreshList}) => {
       console.log("Operación de eliminación cancelada.");
     }
   };
-  
+
   const handleEditUsuario = (usuarioId) => {
-    // Agrega aquí la lógica para editar el usuario
-    console.log(`Editar usuario con ID: ${usuarioId}`);
+    setShowButton(false);
+    setShowRow(true);
+    refreshDynamicDisplayList();
   };
 
-return (
-  <div>
-    {users.length === 0 ? (
-      <p>No hay usuarios disponibles.</p>
-    ) : (
-      <ul>
-        {users.map((user) => (
-          <li className='my-2 p-2 flex justify-between items-center' key={user.uid}>
-            <div>{user.email}</div>
-            <div className="flex gap-2">
-              <button onClick={() => handleEditUsuario(user.id)} className="bg-blue-500 bg-opacity-70 text-white px-2 py-1 rounded-3xl hover:bg-blue-700 hover:bg-opacity-70">
-                Editar
-              </button>
-              <button onClick={() => handleDeleteUsuario(user.id)} className="bg-red-500 bg-opacity-70 text-white px-2 py-1 rounded-3xl hover:bg-red-700 hover:bg-opacity-70">
-                Eliminar
-              </button>
-            </div>
-          </li>
-        ))}
-      </ul>
-    )}
-  </div>
-);
-  };
+  return (
+    <div>
+      {users.length === 0 ? (
+        <p>No hay usuarios disponibles.</p>
+      ) : (
+        <ul>
+          {users.map((user) => (
+            <li className='my-2 p-2 flex justify-between items-center' key={user.uid}>
+              <div>{user.email}</div>
+              <div className="flex gap-2">
+                <button onClick={() => handleEditUsuario(user.id)} className="bg-blue-500 bg-opacity-70 text-white px-2 py-1 rounded-3xl hover:bg-blue-700 hover:bg-opacity-70">
+                  Editar
+                </button>
+                <button onClick={() => handleDeleteUsuario(user.id)} className="bg-red-500 bg-opacity-70 text-white px-2 py-1 rounded-3xl hover:bg-red-700 hover:bg-opacity-70">
+                  Eliminar
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
 
 
 export default UserList;
