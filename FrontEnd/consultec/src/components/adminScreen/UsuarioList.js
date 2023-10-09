fced8049e7885355cb76204656110d4e66b5d054
RonaldCF <ronaldcordero32@gmail.com>
2023-10-08 19:43:59 -0600

    borrado logico de usuarios

--- FrontEnd/consultec/src/components/adminScreen/UsuarioList.js
+++ FrontEnd/consultec/src/components/adminScreen/UsuarioList.js
@@ -1,18 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { app } from '../../firebaseConfig';
-import { getAuth } from 'firebase/auth';
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { getAuth, deleteUser } from 'firebase/auth';
+import { collection, query, getDocs, doc, deleteDoc} from "firebase/firestore";
 import { db } from '../../firebaseConfig';
-
-const handleDeleteUsuario = async (usuarioId) => {
-  // Agrega aquí la lógica para eliminar el usuario
-  console.log(`Eliminar usuario con ID: ${usuarioId}`);
-};
-
-const handleEditUsuario = (usuarioId) => {
-  // Agrega aquí la lógica para editar el usuario
-  console.log(`Editar usuario con ID: ${usuarioId}`);
-};
 
 const UserList = ( {refreshList}) => {
 
@@ -35,7 +25,32 @@ const UserList = ( {refreshList}) => {
       }
     };
     fetchData()
-  }, [db, refreshList])
+  }, [db, refreshList]);
+
+  const handleDeleteUsuario = async (userId) => {
+    // Mostrar un cuadro de diálogo de confirmación antes de eliminar el curso
+    const confirmDelete = window.confirm("¿Estás seguro de que deseas eliminar este usuario?");
+
+    if (confirmDelete) {
+      try {
+
+        const userDocRef = doc(db, "users", userId);
+        await deleteDoc(userDocRef);
+        // Actualiza la lista después de eliminar el curso
+        setUsers(users.filter((user) => user.uid !== userId));
+        alert("Usuario eliminado exitosamente.");
+      } catch (error) {
+        console.error("Error al eliminar el usuario:", error);
+      }
+    } else {
+      console.log("Operación de eliminación cancelada.");
+    }
+  };
+  
+  const handleEditUsuario = (usuarioId) => {
+    // Agrega aquí la lógica para editar el usuario
+    console.log(`Editar usuario con ID: ${usuarioId}`);
+  };
 
 return (
   <div>
@@ -47,10 +62,10 @@ return (
           <li className='my-2 p-2 flex justify-between items-center' key={user.uid}>
             <div>{user.email}</div>
             <div className="flex gap-2">
-              <button onClick={() => handleEditUsuario(user.uid)} className="bg-blue-500 bg-opacity-70 text-white px-2 py-1 rounded-3xl hover:bg-blue-700 hover:bg-opacity-70">
+              <button onClick={() => handleEditUsuario(user.id)} className="bg-blue-500 bg-opacity-70 text-white px-2 py-1 rounded-3xl hover:bg-blue-700 hover:bg-opacity-70">
                 Editar
               </button>
-              <button onClick={() => handleDeleteUsuario(user.uid)} className="bg-red-500 bg-opacity-70 text-white px-2 py-1 rounded-3xl hover:bg-red-700 hover:bg-opacity-70">
+              <button onClick={() => handleDeleteUsuario(user.id)} className="bg-red-500 bg-opacity-70 text-white px-2 py-1 rounded-3xl hover:bg-red-700 hover:bg-opacity-70">
                 Eliminar
               </button>
             </div>
